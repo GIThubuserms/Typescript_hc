932d01a06416c1cde0ba73296b6123f20bf8a9a6
Murtaza0318 <murtazasaleem2004@gmail.com>
2025-02-11 23:41:53 +0500

    Pushing types work

---
+++ 02_basic/Tuples.ts
@@ -0,0 +1,12 @@
+// just an array but fix position 
+
+let rgb:[number,string,number]=[1,"2",3]
+// rgb=[1,"2",4]
+// rgb=[1,5,"3"]
+
+
+
+// TYPESCRIPT array push method cannot catch a tuple type 
+
+
+
--- 02_basic/Union.ts
+++ 02_basic/Union.ts
@@ -1,7 +1,51 @@
+// | union :)
+
 let score:number | string;
 score=4
 score="4"
 
 
-// Now we use types and we
+// Now we use types  Admin and user
 
+type Admin={
+    username:string,
+    age:number
+}
+
+type User ={
+    name:string,
+    age:number
+}
+
+let murtaza1:User|Admin={name:"mm",age:22}
+ murtaza1={username:"mm",age:22}
+
+
+
+
+ // function playing 
+
+ function getdbid(name:number|string){
+   if(typeof(name)==="string"){
+    name.toLocaleString()
+   }
+   if(typeof(name)==="number"){
+    name.toFixed()
+   }
+ }
+getdbid(3)
+
+
+// array 
+
+const data1:number[]=[1,2,3,4,5]
+const data2:string[]=["1","5"]
+const data3:(string|number|boolean)[]=[1,"2",3,"999",5,true]
+
+
+
+// assigning variable 
+
+let aeroplaneseets:"first"|"second"|"third";
+aeroplaneseets="first"
+// aeroplaneseets="fourth"
