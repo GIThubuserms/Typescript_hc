e488b05110e34b6f0b88ec9dedc36a68c2fd80f0
Murtaza0318 <murtazasaleem2004@gmail.com>
2025-02-19 00:05:40 +0500

    Done with 15 mns

--- Advanced/01_ProjectStructure/dist/index.js
+++ Advanced/01_ProjectStructure/dist/index.js
@@ -1,19 +1,32 @@
 "use strict";
 const myname = "Murtaza";
 console.log(myname);
-console.log("Helloworld");
+// console.log("Helloworld");
 // array play
 const myarr = ["M", "U", "R", "T", "A", "Z", "A"];
-myarr.forEach(i => console.log(i));
 const aplha = {
     name: "ede",
     age: 23,
     gender: true,
+    span: () => {
+        console.log("Hello world");
+    }
 };
-const obj = {
-    name: "murtaza",
-    age: 19
+aplha.span;
+let letmyinteface = {
+    name: "dd",
+    age: 32,
+    gender: true,
+    span: () => {
+        console.log("HH");
+    }
 };
-obj.gender = true;
-// obj.age=3
-// Day 1 end on 40 mins
+letmyinteface.span();
+// FUNCTION WITH REST OPERATOR
+const func1 = (num) => {
+    return num;
+};
+const func2 = (...m) => {
+    return m;
+};
+// Day 2 ended with 55 mins
--- Advanced/01_ProjectStructure/src/index.ts
+++ Advanced/01_ProjectStructure/src/index.ts
@@ -1,14 +1,14 @@
 const myname:string="Murtaza"
 console.log(myname);
 
-console.log("Helloworld");
+// console.log("Helloworld");
 
 
 // array play
 
 const myarr:string[]=["M","U","R","T","A","Z","A"]
 
-myarr.forEach(i=>console.log(i))
+// myarr.forEach(i=>console.log(i))
 
 
 // obj and type
@@ -16,7 +16,8 @@ myarr.forEach(i=>console.log(i))
 type Obj={
     name:string,
     readonly age:number,
-    gender?:boolean
+    gender?:boolean,
+    span?:()=>void
 }
 
 type Obj1={
@@ -34,17 +35,56 @@ const aplha:Obj3={
     name:"ede",
     age:23,
     gender:true,
-    
+    span:()=>{
+        console.log("Hello world");     
+    }
 }
 
-const obj:Obj={
-    name:"murtaza",
-    age:19
-}
+ aplha.span  
 
-obj.gender=true
+// const obj:Obj={
+//     name:"murtaza",
+//     age:19
+// }
+
+// obj.gender=true
 // obj.age=3
 
 
 
 // Day 1 end on 40 mins
+
+
+interface muinterface{
+    name:string,
+    readonly age:number,
+    gender?:boolean,
+    span:()=>void
+}
+
+let letmyinteface :muinterface={
+    name:"dd",
+    age:32,
+    gender:true,
+    span:()=>{
+        console.log("HH");
+    }
+}
+
+letmyinteface.span()
+
+
+
+
+// FUNCTION WITH REST OPERATOR
+const func1=(num:number[]):number[]=>{
+    return num
+}
+ 
+const func2=(...m:number[]):number[]=>{
+    return m
+}
+
+
+
+// Day 2 ended with 55 mins
