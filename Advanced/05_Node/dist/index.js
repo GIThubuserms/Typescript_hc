88ec615fe21821540e5e8bca4f72f16a5c9fc76b
Murtaza0318 <murtazasaleem2004@gmail.com>
2025-02-28 19:27:25 +0500

    backe node tsx

---
+++ Advanced/05_Node/dist/index.js
@@ -0,0 +1,9 @@
+// npx tsc -init
+//npm init
+// npm i express 
+import express from 'express';
+const app = express();
+app.listen(4000, () => {
+    console.log("hello from port 4000");
+    console.log("hello from port 4000");
+});
---
+++ Advanced/05_Node/src/index.ts
@@ -0,0 +1,16 @@
+
+// npx tsc -init
+//npm init
+// npm i express 
+
+import express from 'express'
+
+
+const app=express()
+app.listen(4000,()=>{
+    console.log("hello from port 4000");
+    console.log("hello from port 4000");
+    
+})
+
+
